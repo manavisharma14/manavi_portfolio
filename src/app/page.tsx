b7c8629031e076b66221b03d3ec7956c62caafcc
manavisharma14 <manavisharma14@gmail.com>
2025-09-14 01:11:21 -0500

    fixed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative h-screen w-screen">
-      {/* Wallpaper background */}
-
-
-      {/* Content */}
-      <div className="flex flex-col items-center justify-center h-full text-white">
-
-      </div>
+      <p className="mt-2 text-lg text-gray-200 drop-shadow">
+          A macOS-inspired portfolio
+        </p>
     </div>
   );
 }
