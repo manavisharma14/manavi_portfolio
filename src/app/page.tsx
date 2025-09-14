f9c440b589c3c4d1511e044ca93d0bf6a08097de
manavisharma14 <manavisharma14@gmail.com>
2025-09-14 01:34:31 -0500

    basic page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-full text-center">
-      <h1 className="text-4xl font-bold drop-shadow-md">👩‍💻 ManaviOS</h1>
-      <p className="mt-2 text-lg text-gray-200 drop-shadow">
-        A macOS-inspired portfolio website
-      </p>
-    </div>
-  );
+  return <div className="h-screen w-screen" />; // just fills the space
 }
