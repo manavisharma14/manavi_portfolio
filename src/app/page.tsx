72e2b9470dff2955a09683fe0aca46b2a8c2847d
manavisharma14 <manavisharma14@gmail.com>
2025-09-14 01:16:07 -0500

    add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="relative h-screen w-screen">
+    <div className="flex flex-col items-center justify-center h-full text-center">
+      <h1 className="text-4xl font-bold drop-shadow-md">👩‍💻 ManaviOS</h1>
       <p className="mt-2 text-lg text-gray-200 drop-shadow">
-          A macOS-inspired portfolio
-        </p>
+        A macOS-inspired portfolio
+      </p>
     </div>
   );
 }
